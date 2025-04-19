c3880f6716a3a28ccd214a70dd512d23039f7b85
Shelbo <n11014873@qut.edu.au>
2025-04-19 14:01:08 +1000

    Adjusted Create button to export name to new note page

--- src/main/java/com/example/addressbook/HelloApplication.java
+++ src/main/java/com/example/addressbook/HelloApplication.java
@@ -10,13 +10,11 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     // Constants defining the window title and size
     public static final String TITLE = "Address Book";
-    public static final int WIDTH = 640;
-    public static final int HEIGHT = 360;
 
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("create-note-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), WIDTH, HEIGHT);
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle(TITLE);
         stage.setScene(scene);
         stage.show();
--- src/main/java/com/example/addressbook/controller/NewNoteController.java
+++ src/main/java/com/example/addressbook/controller/NewNoteController.java
@@ -1,10 +1,24 @@
 package com.example.addressbook.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
-public class NewNoteController {
+public class NewNoteController extends  CreateNoteController{
 
+    @FXML
     public TextArea noteTextField;
+    public Label currentNoteName;
+    public Button homeButton;
+    public TextField searchBarID;
+
+
+    @FXML
+    public void setLabelText(String text) {
+        currentNoteName.setText(text);
+    }
 
 
 }
