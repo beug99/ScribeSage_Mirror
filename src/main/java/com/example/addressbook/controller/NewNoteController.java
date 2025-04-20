2fb17d9908762260a1abfd6ad3951b11d35dea9f
Shelbo <n11014873@qut.edu.au>
2025-04-20 15:28:06 +1000

    Added Save button and fixed home button, tidied jave file

--- src/main/java/com/example/addressbook/controller/NewNoteController.java
+++ src/main/java/com/example/addressbook/controller/NewNoteController.java
@@ -1,10 +1,17 @@
 package com.example.addressbook.controller;
 
+import com.example.addressbook.HelloApplication;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class NewNoteController extends  CreateNoteController{
 
@@ -13,6 +20,7 @@ public class NewNoteController extends  CreateNoteController{
     public Label currentNoteName;
     public Button homeButton;
     public TextField searchBarID;
+    public Button saveButton;
 
 
     @FXML
@@ -20,5 +28,19 @@ public class NewNoteController extends  CreateNoteController{
         currentNoteName.setText(text);
     }
 
+    @FXML
+    public void onHomeButtonClick(ActionEvent actionEvent) throws IOException  {
+        //TODO Will change the FXML file to the home page once I've merged the project
+        //TODO Currently just goes back to previous page
 
+        Stage stage = (Stage) homeButton.getScene().getWindow();
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("create-note-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setScene(scene);
+    }
+
+    @FXML
+    public void onSaveButtonClick(ActionEvent actionEvent) {
+
+    }
 }
