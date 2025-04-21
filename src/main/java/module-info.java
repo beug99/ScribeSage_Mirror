207ff82a242d164176e37d01096538cf6971d0e7
Shelbo <n11014873@qut.edu.au>
2025-04-21 17:02:43 +1000

    Added favafx-web dependency to POM file. Added requires javafx.web; to module-info
    NOTE: Add to POM first, then update module-info. When adding to pom, Sync the maven project or it plays up

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,7 +3,7 @@ module com.example.addressbook {
     requires javafx.fxml;
     requires java.desktop;
     requires java.sql;
-
+    requires javafx.web;
 
     opens com.example.addressbook to javafx.fxml;
     exports com.example.addressbook;
@@ -11,6 +11,5 @@ module com.example.addressbook {
     opens com.example.addressbook.controller to javafx.fxml;
     exports com.example.addressbook.model;
     opens com.example.addressbook.model to javafx.fxml;
-    opens com.example.addressbook.shellsstufftodelete to javafx.fxml;
-    exports com.example.addressbook.shellsstufftodelete;
+
 }
