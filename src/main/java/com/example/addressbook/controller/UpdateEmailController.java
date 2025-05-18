80e81ef34eb7ae8d2e60cfc19de58adb3f5baa63
Shelbo <n11014873@qut.edu.au>
2025-05-18 11:02:06 +1000

    Added Javadoc comments to UpdateEmailController.java

--- src/main/java/com/example/addressbook/controller/UpdateEmailController.java
+++ src/main/java/com/example/addressbook/controller/UpdateEmailController.java
@@ -22,6 +22,11 @@ import java.io.IOException;
 import java.util.List;
 import java.util.regex.Pattern;
 
+/**
+ * This controller class manages the update email page for the application.
+ * The scene will load through the updateEmail-view.fxml. It provides methods for users
+ * to alter their email address that is stored on the database.
+ */
 public class UpdateEmailController {
 
     @FXML private PasswordField currentPassword;
@@ -29,11 +34,22 @@ public class UpdateEmailController {
     @FXML private Button backButton;
     @FXML private INoteDAO noteDAO;
 
+    /**
+     * Constructs an instance of a note Data Access Object (INoteDAO) and assigns it to noteDAO.
+     */
     public UpdateEmailController()
     {
         noteDAO = new SqliteNoteDAO();
     }
 
+    /**
+     * This method assesses the user input from the password and new email fields and validates
+     * if the changes are permitted. The current users password must be accurate, the new email
+     * address must be in the correct syntax. If either of these are incorrect, the user is alerted
+     * to the reason why the change of email address has failed. If successful, the email address in
+     * the database is updated.
+     * @throws IOException An error appears or the page fails to load.
+     */
     @FXML
     private void onConfirmUpdateEmail() throws IOException {
         String currentPwd = currentPassword.getText();
@@ -69,16 +85,36 @@ public class UpdateEmailController {
         }
     }
 
+    /**
+     * A method that analyses the email entered by the user. If the email is written in the correct
+     * syntax it is valid, else it is invalid.
+     * @param email The new email entered by the user.
+     * @return True if valid, False if invalid.
+     */
     private boolean isValidEmail(String email) {
         String emailRegex = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$";
         Pattern pattern = Pattern.compile(emailRegex);
         return pattern.matcher(email).matches();
     }
+
+    /**
+     * This method returns the user to the previous Update Details page. If the 'Confirm Update'
+     * button has not been clicked, the updates are discarded.
+     * @throws IOException An error appears or the page does not load.
+     */
     public void onBackToDetails() throws IOException  {
             Stage stage = (Stage) backButton.getScene().getWindow();
             SceneLoader.switchScene(stage, "updateDetails-view.fxml", false);
-        }
+    }
 
+    /**
+     * This method manages the Error alert that appears when interacting with the Update Email page.
+     * It requires input parameters of alert type (Error, Information etc.), the error title and
+     * content of the error which will display to the user once the method is called.
+     * @param type The type of the alert.
+     * @param title The title of the alert.
+     * @param content The content of the alert.
+     */
     private void showAlert(Alert.AlertType type, String title, String content) {
         Alert alert = new Alert(type);
         alert.setTitle(title);
@@ -86,5 +122,4 @@ public class UpdateEmailController {
         alert.setContentText(content);
         alert.showAndWait();
     }
-
 }
