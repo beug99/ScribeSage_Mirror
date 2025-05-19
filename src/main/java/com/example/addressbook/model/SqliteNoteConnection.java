dafe471ef7253c485666d0f28b13c90f25c6b495
Shelbo <n11014873@qut.edu.au>
2025-05-19 15:04:52 +1000

    Added Javadoc comments to SqliteNoteConnection.java and SqliteUserConnection.java

--- src/main/java/com/example/addressbook/model/SqliteNoteConnection.java
+++ src/main/java/com/example/addressbook/model/SqliteNoteConnection.java
@@ -3,10 +3,19 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+/**
+ * A model class that represents the connection to the Note database. This class is
+ * implemented by the {@link SqliteNoteConnection} class to connect to save, view, edit and
+ * delete items from the database.
+ */
 public class SqliteNoteConnection {
     private static Connection instance = null;
     private static SQLException lastException = null; // storing last exception for testing
 
+    /**
+     * A constructor that initialises the connection to the database.
+     * @param DbName The name of the database.
+     */
     private SqliteNoteConnection(String DbName) {
         String url = "jdbc:sqlite:" + DbName;
         try {
@@ -16,6 +25,11 @@ public class SqliteNoteConnection {
         }
     }
 
+    /**
+     * A method that provides global access to the instance of the database connection.
+     * @param DbName The name of the database.
+     * @return A handle to the instance of the database connection.
+     */
     public static Connection getInstance(String DbName) {
         if (instance == null) {
             new SqliteNoteConnection(DbName);
@@ -23,20 +37,30 @@ public class SqliteNoteConnection {
         return instance;
     }
 
-    // testing method
+
+    /**
+     * A testing method that resets the connection to the database.
+     * @param url The url connection to be reset.
+     */
     public static void resetConnection(String url) {
         instance = null;
         lastException = null;
         new SqliteNoteConnection(url);
     }
 
-    // get the exception and store it for debugging
+    /**
+     * A testing method that returns the latest SQL Exception.
+      * @return The exception.
+     */
     public static SQLException getLastException() {
         lastException = new SQLException();
         return lastException;
     }
 
-    // close connection
+    /**
+     * A testing method that closes the connection to the database.
+     * @param url The url of the database which connection will be closed.
+     */
     public static void closeConnection(String url){
         SqliteNoteConnection.closeConnection("notes.db");
     }
